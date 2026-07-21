cd7ad84cf609ac78455c520cf6862edaf7a51a5f
Gabriel Dellatore E. <dellatoregabriel8@gmail.com>
2026-07-21 18:53:21 -0300

    integrating Book Service into database with Spring Data JPA and Hibernate

--- src/main/java/br/com/erudio/controller/BookController.java
+++ src/main/java/br/com/erudio/controller/BookController.java
@@ -2,31 +2,30 @@ package br.com.erudio.controller;
 
 import br.com.erudio.environment.InstanceInformationService;
 import br.com.erudio.model.Book;
+import br.com.erudio.repository.BookRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 @RestController
 @RequestMapping("/book-service")
 public class BookController {
 
     private final InstanceInformationService instanceInformationService;
+    private final BookRepository repository;
 
-    public BookController(InstanceInformationService instanceInformationService) {
+    public BookController(InstanceInformationService instanceInformationService, BookRepository repository) {
         this.instanceInformationService = instanceInformationService;
+        this.repository = repository;
     }
 
     @GetMapping("{id}/{currency}")
     public Book findBook(@PathVariable Long id, @PathVariable String currency) {
-        return new Book(id,
-                "",
-                "Java EE 8",
-                new Date(),
-                29.90,
-                currency,
-                instanceInformationService.retrievePort());
+        Book book = repository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+        book.setCurrency(currency);
+        book.setEnvironment(instanceInformationService.retrievePort());
+
+        return book;
     }
 }
--- src/main/java/br/com/erudio/model/Book.java
+++ src/main/java/br/com/erudio/model/Book.java
@@ -1,19 +1,40 @@
 package br.com.erudio.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+
+@Entity(name = "book")
 public class Book implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "author")
     private String author;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "launch_date")
+    @Temporal(TemporalType.DATE)
     private Date launchDate;
+
+    @Column(name = "price")
     private Double price;
+
+
+    @Transient
     private String currency;
+
+    @Transient
     private String environment;
 
     public Book() {
